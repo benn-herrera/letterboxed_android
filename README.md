6dd606c98ead9e55f79fc6269777f7b08af1fb98
Benn Herrera <benn.herrera@pm.me>
2024-10-28 13:33:48 -0700

    Broke out SolverUI and added README

---
+++ README.md
@@ -0,0 +1,45 @@
+Solver for NYT Word Puzzle Letter Boxed
+=======================================
+* [Game Link](https://www.nytimes.com/puzzles/letter-boxed)
+* [Tutorial](https://wordfinder.yourdictionary.com/blog/nyts-letter-boxed-a-quick-guide-to-the-fan-favorite-puzzle/)
+* [Letterboxed Answers](https://letterboxedanswers.com) is a good source for test puzzles
+* Regardless of the number of target words specified in the instructions for a given day there is always a two word solution.
+
+
+TL;DR
+------
+* The puzzle is a box made of 12 unique letters, 3 to a side
+* Use all the letters in as few words as possible
+* Each successive letter of a word must come from a different box side
+* Each successive word must start with the last letter of the preceding word
+
+Example
+-------
+ s  a  d
+i       t
+c       n
+g       o
+ r  u  h
+
+* standing -> grouch would be one solution to this puzzle
+  * There is always at least one two-word solution
+  * There may be a one-word solution
+* Examples of invalid words and solutions for this puzzle
+  * cast: uses a and s in succession from the same side of the box
+  * hogging: uses a doubled letter hits the same side twice in a row
+  * grousing -> chat: first letter of second word is not the last letter of the first
+
+Usage
+-----
+Enter the letters of the puzzle sides in the text field and hit "Solve"
+
+* First use downloads a word list. Successive uses read the word list from local storage.
+
+Sample Puzzles
+--------------
+* vrq wue isl dmo
+* dng ruh iae ftm
+* tdg nal kie ubh
+* yde lis frn ovt
+* rpj cit owl aks
+* eid rah tyo bmn
--- app/src/main/java/com/tinybitsinteractive/lbsolver/MainActivity.kt
+++ app/src/main/java/com/tinybitsinteractive/lbsolver/MainActivity.kt
@@ -5,37 +5,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tinybitsinteractive.lbsolver.ui.theme.LetterboxedSolverTheme
-import java.lang.Thread.sleep
 
 internal fun cleanSides(sides: String): String {
     var cleaned = ""
@@ -78,106 +52,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun SolverUI(modifier: Modifier = Modifier) {
-    var sidesTFV by remember {
-        // mutableStateOf(TextFieldValue("", selection = TextRange(0)))
-        mutableStateOf(TextFieldValue("slq xti fua eno", selection = TextRange(15)))
-    }
-
-    var solutions by remember { mutableStateOf( "") }
-    var working by remember { mutableIntStateOf(0) }
-    val context = LocalContext.current
-
-    Column(
-        modifier = modifier
-    ) {
-        Text(
-            modifier = Modifier
-                .height(24.dp)
-                .align(Alignment.CenterHorizontally),
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp,
-            text = "Letterboxed Solver"
-        )
-        Text(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-            text = "Sides [e.g.: abc def ghi jkl]"
-        )
-        Row (
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-        )
-        {
-            TextField(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                value = sidesTFV,
-                onValueChange = { tfv ->
-                    val cleaned = cleanSides(tfv.text)
-                    sidesTFV = TextFieldValue(
-                        cleaned,
-                        TextRange(cleaned.length)
-                    )
-                    if (cleaned.length != 15) {
-                        synchronized(solutions) {
-                            solutions = ""
-                            working = 0
-                        }
-                    }
-                }
-            )
-            Button(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = {
-                    synchronized(solutions) {
-                        solutions = ""
-                        working = 1
-                    }
-                    val solver = Thread(
-                        PuzzleSolver(
-                            cacheDir = context.cacheDir.toPath(),
-                            box = sidesTFV.text
-                        ) { results ->
-                            synchronized(solutions) {
-                                solutions = results
-                                working = 0
-                            }
-                        })
-                    solver.start()
-                },
-                enabled = sidesTFV.text.length == 15 && solutions.isEmpty() && working == 0,
-                content = {
-                    Text("Solve")
-                }
-            )
-        }
-        synchronized(solutions) {
-            if (working > 0 || solutions.isNotEmpty()) {
-                Text("Solutions:")
-                Text(
-                    solutions.ifEmpty { "*".repeat(++working % 12) },
-                    modifier = Modifier
-                        .border(width = 1.dp, color = Color(0.0f, 0.0f, 0.0f))
-                        .padding(start = 5.dp)
-                        .fillMaxWidth()
-                        .verticalScroll(rememberScrollState())
-                )
-                if (solutions.isEmpty()) {
-                    sleep(30)
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SolverUIPreview() {
-    LetterboxedSolverTheme {
-        SolverUI()
     }
 }
